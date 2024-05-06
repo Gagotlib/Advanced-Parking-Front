61d3a80fc2d8ad5bfe0d08187b3c225e67c05410
Gabriel Gotlib <gagotlib@gmail.com>
2024-05-06 22:58:24 +0200

    correccion de color

--- advanced-parking/src/app/page.tsx
+++ advanced-parking/src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Landing() {
 	return (
-		<main className='pt-10 py-4 min-h-screen flex flex-col bg-blue-300 items-center'>
+		<main className='pt-10 py-4 min-h-screen flex flex-col bg-duck-yellow items-center'>
 			<div className=' flex flex-col flex-1 items-center gap-6 lg:flex-row sm:px-10 lg:justify-around w-full max-w-[1200px]'>
 				<Image src='/advanced_parking.png' alt='app logo' className=' min-w-[200px] min-h-[200px]  rounded-full sm:w-[550px] ' width={400} height={400} />
 				<div className=' flex flex-col items-center gap-6'>
@@ -20,7 +20,7 @@ export default function Landing() {
 					<Link href='/home'>continue as guest</Link>
 				</div>
 			</div>
-			<div className='text-md sm:text-md text-center '>
+			<div className='text-md sm:text-md text-center  '>
 				<p>Advanced Parking &copy; 2024. All Rights Reserved.</p>
 			</div>
 		</main>
--- advanced-parking/tailwind.config.ts
+++ advanced-parking/tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			},
+			colors: {
+				'duck-yellow': '#FFCC00'
+			}
+		}
+	},
+	plugins: []
+}
+export default config
+// theme: {
+//     fontFamily: {
+//       'sarala': ['Sarala', 'sans-serif']
+//     },
+//     colors: {
+//       'charcoal': '#fbe5c0',
+//       'coral': '#f88379',
+//       'serenity': '#d0e6f0',
+//       'charcoal2': '#4c5d70'
+//     },
+//     extend: {},
+//   }
