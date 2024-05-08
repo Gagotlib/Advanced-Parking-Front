81be2eec6305a49659a7f455855be4ea64ee3d5c
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-08 11:56:17 +0200

    Update LandingPAge

--- advanced-parking/src/app/page.tsx
+++ advanced-parking/src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 
 export default function Landing() {
 	return (
-		<main className='pt-10 py-4 min-h-screen flex flex-col bg-landing_page bg-cover items-center'>
-			<div className=' flex flex-col flex-1 items-center gap-6 lg:flex-row sm:px-10 lg:justify-around w-full max-w-[1200px]'>
+		<main className='bg-erieblack pt-10 py-4 min-h-screen flex flex-col items-center'>
+			<div className='flex flex-col flex-1 items-center gap-6 lg:flex-row sm:px-10 lg:justify-around w-full max-w-[1200px]'>
 				<Image
 					src='/advanced_parking_1.webp'
 					alt='advanced parking app'
@@ -13,19 +13,23 @@ export default function Landing() {
 					width={400}
 					height={400}
 				/>
-				<div className=' flex flex-col items-center gap-6'>
-					<h1 className='text-white text-lg font-bold'>Welcome to your preferred parking! Your space awaits you.</h1>
+				<div className='flex flex-col items-center gap-10'>
+					<h1
+						className='text-ghostwhite text-3xl font-bold'>
+						Welcome to your preferred parking!
+						<br />
+						Your space awaits you.
+					</h1>
 					<div className='flex flex-col'>
 						<Link href='/register' className='flex flex-col '>
 							<CreateAccountButton />
 						</Link>
 						<GoogleButton />
 					</div>
-
-					<Link href='/home' className='text-white'>Continue as guest</Link>
+					<Link href='/home' className='text-ghostwhite font-medium text-base'>Continue as guest</Link>
 				</div>
 			</div>
-			<div className='text-md text-white sm:text-md text-center text-lg '>
+			<div className='text-md text-ghostwhite font-medium sm:text-md text-center text-xl'>
 				<p className=''>Advanced Parking &copy; 2024. All Rights Reserved.</p>
 			</div>
 		</main>
