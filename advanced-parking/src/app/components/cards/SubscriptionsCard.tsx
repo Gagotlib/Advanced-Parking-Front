037f72c0333c5f5174b463c83dae409038ffc990
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-08 18:14:37 +0200

    Uptade Files and Navbar

--- advanced-parking/src/app/components/cards/SubscriptionsCard.tsx
+++ advanced-parking/src/app/components/cards/SubscriptionsCard.tsx
@@ -14,9 +14,9 @@ interface ISubscription {
 export const SubscriptionsCard = ({ subscription }: { subscription: ISubscription }) => {
 	const color = subscription.color
 	return (
-		<div className='flex flex-col sm:flex-row items-start justify-start  rounded-3xl w-11/12 h-40 shadow-2xl'>
-			<Link href={`/subscriptions/${subscription.name}`} className='w-full'>
-				<div className='relative flex justify-center sm:justify-around items-center w-full h-40  sm:flex-row sm:w-full  sm:[&>img]:hover:filter-none '>
+		<div className='flex flex-col items-start justify-start rounded-3xl w-12/12 h-40 sm:h-96 shadow-2xl'>
+			<Link href={`/subscriptions/${subscription.name}`} className='w-full flex justify-center'>
+				<div className='relative flex flex-col justify-center sm:justify-around items-center w-full h-40 sm:flex-col sm:w-full sm:gap-8 sm:[&>img]:hover:filter-none'>
 					<Image
 						src={subscription.img_href}
 						alt={subscription.name}
@@ -24,11 +24,15 @@ export const SubscriptionsCard = ({ subscription }: { subscription: ISubscriptio
 						height={200}
 						className='shadow-xl object-cover rounded-3xl h-full filter-none sm:filter grayscale brightness-50 blur-[2px] hover:filter-none'
 					/>
-					<div className='absolute sm:flex-col sm:relative sm:text-black text-white text-center  '>
+					<div className='absolute sm:flex-col sm:relative sm:text-black text-white text-center	'>
 						<p className='font-bold text-3xl '>{subscription.name}</p>
 						<p className=''>{subscription.shortDescription}</p>
-						<p className=''>${subscription.price} / mes</p>
+						<p className='text-3xl font-bold'>${subscription.price} / Month</p>
 					</div>
+					<button
+						className="hidden sm:flex text-ghostwhite bg-yaleblue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-silver font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+						Ver detalle
+					</button>
 				</div>
 			</Link>
 		</div>
