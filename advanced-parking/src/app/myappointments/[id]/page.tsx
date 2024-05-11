daa894d6f8775a3a2a5318ee13bbb83504c48eb2
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-11 18:56:43 +0200

    Page BookingDetail

--- advanced-parking/src/app/myappointments/[id]/page.tsx
+++ advanced-parking/src/app/myappointments/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import BookingDetail from './BookingDetail'
 
 const page = ({ params }: { params: { id: string } }) => {
 	return (
 		<div className='flex flex-col min-h-screen pt-24'>
 			<h1>appointments Detail</h1>
 			<h1>{params.id}</h1>
+			<BookingDetail />
 		</div>
 	)
 }
