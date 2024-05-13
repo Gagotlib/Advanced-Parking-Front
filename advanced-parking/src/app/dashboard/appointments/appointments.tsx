19b9d61e3587da132090b39e3a77b0189f66539f
Gabriel Gotlib <gagotlib@gmail.com>
2024-05-13 22:15:37 +0200

    commit 4

--- advanced-parking/src/app/dashboard/appointments/appointments.tsx
+++ advanced-parking/src/app/dashboard/appointments/appointments.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 type Props = {}
 
-export const page = (props: Props) => {
+export const appointments = (props: Props) => {
 	return (
 		<div className='flex flex-col min-h-screen md:pt-8'>
 			<h1>Esto es lo correspondiente a appointments</h1>
 		</div>
 	)
 }
-export default page
+
+export default appointments
