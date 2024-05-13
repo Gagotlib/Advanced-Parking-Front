2eb4a249548bcb713c9f904427a558efa76d5da2
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-13 18:10:15 +0200

    Update Files

--- advanced-parking/src/app/subscriptions/[slug]/SubscriptionDetails.tsx
+++ advanced-parking/src/app/subscriptions/[slug]/SubscriptionDetails.tsx
@@ -11,28 +11,28 @@ export const SubscriptionDetails = ({ params }: { params: { slug: string } }) =>
 	return (
 		<div className='flex flex-col min-h-screen pt-24'>
 			<div className='flex flex-col p-4 m-0 items-center justify-start gap-4 text-center'>
-				<h1>Subscripción {subscription?.name}</h1>
-				<p>{subscription?.longDescription}</p>
+				<h1 className='text-4xl sm:text-6xl'>Suscription {subscription?.name}</h1>
+				<p className='text-xl '>{subscription?.longDescription}</p>
 				<ul>
-					<li className='flex gap-2 items-center'>
+					<li className='flex gap-2 items-center mb-4 w-full justify-start'>
 						<CheckIcon />
-						<p>Lugar asegurado</p>
+						<p>100% insured parking in the reservation</p>
 					</li>
-					<li className='flex gap-2 items-center'>
+					<li className='flex gap-2 items-center mb-4 w-full justify-start'>
 						<CheckIcon />
-						<p>Seguridad las 24hs</p>
+						<p >Security 24hr</p>
 					</li>
-					<li className='flex gap-2 items-center'>
+					<li className='flex gap-2 items-center mb-4 w-full justify-start'>
 						<CheckIcon />
-						<p>Ingreso y Salida automatizada</p>
+						<p>Automated check-in and check-out</p>
 					</li>
 				</ul>
-				<p>${subscription?.price} por mes</p>
+				<p>${subscription?.price} per Month</p>
 				<button
 					type='button'
 					className='py-2.5 px-5 my-8 me-2 text-md sm:text-2xl font-medium text-gray-900 focus:outline-none bg-duck-yellow rounded-lg border border-gray-200 hover:bg-yellow-300  focus:z-10 focus:ring-4 focus:ring-gray-100 shadow-xl'
 				>
-					<Link href=''>Contratar</Link>
+					<Link href=''>Hire</Link>
 				</button>
 				<BackToSubscriptionsButton />
 			</div>
