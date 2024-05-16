82df71cc502ead301894ff4a479caef6b97871a0
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-16 20:47:20 +0200

    Update Page Cancel Payment and fixed files

--- advanced-parking/src/app/cancel/Cancel.tsx
+++ advanced-parking/src/app/cancel/Cancel.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { ContactButton } from '../components/buttons/Buttons'
+import { BackToHomeButton, ContactButton } from '../components/buttons/Buttons'
+import Image from 'next/image'
 
 const Cancel = () => {
 	return (
-		<div className='flex flex-col items-center min-h-screen pt-24'>
-			<div className=' flex flex-col items-center gap-2'>
-				<h1>Appointment not approved </h1>
-				<p>Payment could not be processed</p>
-				<p>Please try again</p>
-				<p>If the problem persists please contact us here</p>
-
-				<ContactButton />
-				<p>Thank you for using our service</p>
-
+		<div className='flex flex-col sm:grid sm:grid-cols-2 justify-center items-center min-h-screen pt-20 px-4'>
+			<div>
+				<Image
+					src='/cancelPayment.webp'
+					alt='Cancel Image Payment'
+					className='flex object-cover w-[200px] h-[200px] sm:w-full sm:h-full'
+					width={500}
+					height={500}
+					priority
+				/>
+			</div>
+			<div className='flex flex-col text-center items-center gap-6'>
+				<h1 className='text-5xl sm:text-7xl font-extrabold text-red-700 text-end'>
+					Ups!!
+				</h1>
+				<p>We´re sorry, an error occurred during the payment process.
+				</p>
+				<p>
+					It could be a temporary error or incorrect information. You can try again.
+				</p>
+				<div className='flex gap-4'>
+					<BackToHomeButton />
+					<ContactButton />
+				</div>
+				<p>Contact us for personalized assistance.</p>
 				<p>Advanced Parking Support Team</p>
 			</div>
 		</div>
