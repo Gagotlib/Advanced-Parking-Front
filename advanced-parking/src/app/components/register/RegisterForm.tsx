4d5d44ed0232f58e93b36ba7023baad51e51a790
Gabriel Gotlib <gagotlib@gmail.com>
2024-05-17 15:58:04 +0200

    ruta

--- advanced-parking/src/app/components/register/RegisterForm.tsx
+++ advanced-parking/src/app/components/register/RegisterForm.tsx
@@ -10,7 +10,7 @@ import React, { useEffect, useState } from 'react'
 
 export const RegisterForm = () => {
 	const rute = process.env.NEXT_API_URL
-	console.log('ruta', rute)
+	console.log('ruta NEXT_API_URL', rute)
 
 	const router = useRouter()
 	const [showToast, setShowToast] = useState(false)
