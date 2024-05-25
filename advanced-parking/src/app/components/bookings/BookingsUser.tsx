eccfb0e5a77f9a5505076822f6ddddbc235974a9
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-25 18:43:00 +0200

    Updated View Profile

--- advanced-parking/src/app/components/bookings/BookingsUser.tsx
+++ advanced-parking/src/app/components/bookings/BookingsUser.tsx
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TicketProfile from '../profile_file/TicketProfile';
 
 interface IApointment {
-	date: string
-	duration: string
-	id: string
-	is_parked: boolean
-	license_plate: string
+	date: string;
+	duration: string;
+	id: string;
+	is_parked: boolean;
+	license_plate: string;
 	parking_lot: {
-		id: string
-		lat: string
-		lng: string
-		location: string
-		name: string
-		slots_stock: number
-	}
-	slot_number: string
-	status: string
-	time: string
-	total: number
+		id: string;
+		lat: string;
+		lng: string;
+		location: string;
+		name: string;
+		slots_stock: number;
+	};
+	slot_number: string;
+	status: string;
+	time: string;
+	total: number;
 }
 
 interface BookingsUserProps {
-	userAppointments: IApointment[] | null
+	userAppointments: IApointment[] | null;
 }
 
 const BookingsUser: React.FC<BookingsUserProps> = ({ userAppointments }) => {
+
+	const [showTicket, setShowTicket] = useState<{ show: boolean, id: string | null }>({ show: false, id: null });
+
+	const handleClick = (id: string) => {
+		setShowTicket({ show: !showTicket.show, id });
+	};
 
 	return (
 		<div className='flex flex-col'>
@@ -35,41 +42,41 @@ const BookingsUser: React.FC<BookingsUserProps> = ({ userAppointments }) => {
 				<table className='w-full text-center text-erieblack table-auto border-collapse border border-erieblack/90'>
 					<thead className='text-xs text-erieblack font-bold uppercase md:text-md bg-silver'>
 						<tr>
-							<th scope='col' className='px-6 py-2 border border-erieblack/90'>
-								Parking Lot
-							</th>
-							<th scope='col' className='px-6 py-2 border border-erieblack/90'>
-								Date
-							</th>
-							<th scope='col' className='px-6 py-2 border border-erieblack/90'>
-								Hour
-							</th>
-							<th scope='col' className='px-6 py-2 border border-erieblack/90'>
-								License Plate
-							</th>
-							<th scope='col' className='px-6 py-2 border border-erieblack/90'>
-								Status
-							</th>
+							<th scope='col' className='px-6 py-2 border border-erieblack/90'>Parking Lot</th>
+							<th scope='col' className='px-6 py-2 border border-erieblack/90'>Date</th>
+							<th scope='col' className='px-6 py-2 border border-erieblack/90'>Hour</th>
+							<th scope='col' className='px-6 py-2 border border-erieblack/90'>Status</th>
+							<th scope='col' className='px-6 py-2 border border-erieblack/90'>Ticket</th>
 						</tr>
 					</thead>
 					<tbody>
 						{userAppointments?.map((appointment: IApointment) => (
-
 							<tr key={appointment.id} className='bg-ghostwhite'>
-								<th scope='row' className='px-3 py-4 font-medium text-erieblack whitespace-nowrap border-collapse border border-erieblack'>
+								<th scope='row' className='px-3 py-4 font-medium text-erieblack border-collapse border border-erieblack'>
 									{appointment.parking_lot?.name}
 								</th>
 								<td className='px-3 py-4 border border-erieblack/90'>{appointment.date}</td>
 								<td className='px-3 py-4 border border-erieblack/90'>{appointment.time}</td>
-								<td className='px-3 py-4 border border-erieblack/90'>{appointment.license_plate}</td>
 								<td className='px-3 py-4 border border-erieblack/90'>{appointment.status}</td>
+								<td className='px-1 py-1 border border-erieblack/90'>
+									<button
+										type='button'
+										className='py-2 px-2 text-base font-medium text-ghostwhite focus:outline-none bg-yaleblue rounded-lg border border-silver hover:bg-ghostwhite hover:text-yaleblue focus:z-10 focus:ring-2 focus:ring-yaleblue/50'
+										onClick={() => handleClick(appointment.id)}
+									>
+										See Ticket
+									</button>
+								</td>
 							</tr>
 						))}
 					</tbody>
 				</table>
+				{showTicket.show && showTicket.id && (
+					<TicketProfile params={{ id: showTicket.id }} />
+				)}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default BookingsUser
+export default BookingsUser;
---
+++ advanced-parking/src/app/components/profile_file/TicketProfile.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import axios from 'axios';
+import React, { Suspense, useEffect, useState } from 'react';
+import QRGenerator from '../qrcode/QRGenerator';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+interface IAppointment {
+  date: string;
+  duration: string;
+  id: string;
+  is_parked: boolean;
+  license_plate: string;
+  parking_lot: {
+    id: string;
+    lat: string;
+    lng: string;
+    location: string;
+    name: string;
+    slots_stock: number;
+  };
+  slot: {
+    id: string;
+    slot_number: number;
+    slot_status: string;
+  };
+  slot_number: string;
+  status: string;
+  time: string;
+  total: number;
+  user: {
+    email: string;
+    id: string;
+    image: string | null;
+    name: string;
+    password: string;
+    phone: string;
+    role: string;
+    status: string;
+  };
+}
+
+const TicketProfile = ({ params }: { params: { id: string } }) => {
+  const rute = process.env.NEXT_PUBLIC_BACK_API_URL;
+  const [appointmentDetails, setAppointmentDetails] = useState<IAppointment | null>(null);
+  const [observer, setObserver] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    axios
+      .get(`${rute}/appointments/${params.id}`, {
+        headers: {
+          Authorization: `Bearer: ${token}`
+        }
+      })
+      .then(({ data }) => setAppointmentDetails(data))
+      .catch(error => console.error('Error fetching appointment details:', error));
+  }, [observer]);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className="fixed top-28 left-0 w-full h-full flex items-center justify-center z-10 ">
+      <div className="relative p-1">
+
+        <Suspense fallback={<h1>Loading...</h1>}>
+          {appointmentDetails ? (
+            <div className='flex flex-col min-h-screen px-4 pb-4 lg:pt-10'>
+              <div className='flex justify-center items-center'>
+                <button
+                  className="text-ghostwhite hover:text-silver border border-yaleblue rounded-full w-fit p-3 flex items-center justify-center bg-yaleblue"
+                  onClick={handleClose}
+                >
+                  <FontAwesomeIcon icon={faTimes} />
+                </button>
+              </div>
+              <div className='bg-ghostwhite border border-silver/80 rounded-lg shadow-lg shadow-erieblack/80 px-6 mx-auto mt-2'>
+
+
+                <div className='mb-8'>
+                  <h2 className='text-md font-bold mb-4'>
+                    Ticket: <span className='text-erieblack/80 text-sm'>{appointmentDetails.id}</span>
+                  </h2>
+                  <p className='text-erieblack/80 text-sm'>Parking: {appointmentDetails.parking_lot.name}</p>
+                  <p className='text-erieblack/80 text-sm'>Check-in date: {appointmentDetails.date}</p>
+                  <p className='text-erieblack/80 text-sm'>Check-in hour: {appointmentDetails.time}</p>
+                  <div className='flex justify-center items-center'>
+                    <QRGenerator />
+                  </div>
+                  <div className='flex justify-around'>
+                    <p className='text-erieblack/80 text-sm'>Slot: {appointmentDetails.slot_number}</p>
+                    <p className='text-erieblack/80 text-sm'>Duration: {appointmentDetails.duration} hrs.</p>
+                    <p className='text-erieblack/80 text-sm'>License Plate: {appointmentDetails.license_plate}</p>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+          ) : (
+            <div className='flex flex-col min-h-screen md:pt-6'>
+              <h3>Loading...</h3>
+            </div>
+          )}
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default TicketProfile;
