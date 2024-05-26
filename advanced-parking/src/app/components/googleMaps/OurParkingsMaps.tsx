8dd6e5499067ccf685a56f9c2758fcbd517d9e95
DIbarguenCurrea <dibarguenc@gmail.com>
2024-05-26 14:14:51 +0200

    Update files

--- advanced-parking/src/app/components/googleMaps/OurParkingsMaps.tsx
+++ advanced-parking/src/app/components/googleMaps/OurParkingsMaps.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow, MapControl, ControlPosition } from '@vis.gl/react-google-maps'
 import { CustomZoomControl } from './CustomZoomControl';
 import { useAuth } from '@/app/context/AuthContext';
@@ -13,10 +13,39 @@ function OurParkingsMaps() {
   const [openIndex, setOpenIndex] = useState<number | null>(null)
   const [controlPosition, setControlControlPosition] = useState<ControlPosition>(ControlPosition.LEFT_BOTTOM);
 
-  const defaultPosition = {
-    lat: -34.590422,
-    lng: -58.392357
+  const latLocalStorage = localStorage.getItem('lat')
+  const lngLocalStorage = localStorage.getItem('lng')
+
+  const positionDefault = {
+    lat: Number(latLocalStorage) - 34.590422,
+    lng: Number(lngLocalStorage) - 58.392357
   }
+
+  const [defaultPosition, setDefaultPosition] = useState({ lat: positionDefault.lat, lng: positionDefault.lng })
+  // Solicita permisos de geolocalización
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords
+          console.log('Tomando posicion del navegador', latitude, longitude)
+          setDefaultPosition({ lat: latitude, lng: longitude })
+          localStorage.setItem('lat', latitude.toString())
+          localStorage.setItem('lng', longitude.toString())
+        },
+        (error) => {
+          console.error('Error obteniendo la geolocalización: ', error)
+        }
+      )
+    } else {
+      setDefaultPosition({ lat: positionDefault.lat, lng: positionDefault.lng })
+      console.error('La geolocalización no es soportada por este navegador.')
+    }
+    console.log('Default', defaultPosition)
+    console.log(typeof defaultPosition.lat)
+    console.log(typeof defaultPosition.lng)
+  }, [])
+
 
   const parkingValues = allParkings?.map((parking) => {
     return {
@@ -41,6 +70,9 @@ function OurParkingsMaps() {
           gestureHandling={'greedy'}
           defaultCenter={defaultPosition}
         >
+          {/* <AdvancedMarker
+            position={defaultPosition}>
+          </AdvancedMarker> */}
           <MapControl position={ControlPosition.TOP_LEFT}>
             <div
               style={{
@@ -55,10 +87,11 @@ function OurParkingsMaps() {
             zoom={zoom}
             onZoomChange={zoom => setZoom(zoom)}
           />
+          <AdvancedMarker position={defaultPosition}></AdvancedMarker>
           {parkingValues?.map((parking, index) => (
             <div key={index}>
               <AdvancedMarker
-                position={parking.position}
+                position={parking?.position}
                 onClick={() => setOpenIndex(index)}
               >
                 <Pin
@@ -67,12 +100,12 @@ function OurParkingsMaps() {
               </AdvancedMarker>
               {openIndex === index && (
                 <InfoWindow
-                  position={parking.position}
+                  position={parking?.position}
                   onCloseClick={() => setOpenIndex(null)}
                 >
-                  <p className='font-bold'>Parking {parking.name}</p>
-                  <p className='font-light'>{parking.address}</p>
-                  <Link href={`/ourparkings/${parking.id}`} className='underline decoration-yaleblue text-yaleblue'>
+                  <p className='font-bold'>Parking {parking?.name}</p>
+                  <p className='font-light'>{parking?.address}</p>
+                  <Link href={`/ourparkings/${parking?.id}`} className='underline decoration-yaleblue text-yaleblue'>
                     View details
                   </Link>
                 </InfoWindow>
